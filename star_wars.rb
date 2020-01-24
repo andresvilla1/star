ae864daa8c6fad3f227f178e92d750bbfa7473dc
andresvilla1 <37849309+andresvilla1@users.noreply.github.com>
2020-01-24 14:51:32 -0800

    Add files via upload

---
+++ star_wars.rb
@@ -0,0 +1,85 @@
+# Welcome to Sonic Pi v3.1
+3.times do
+  play :G5
+  sleep 0.6
+end
+
+2.times do
+  play :Ef5
+  sleep 0.45
+  play :Bf5
+  sleep 0.15
+  play :G5
+  sleep 0.6
+end
+sleep 0.7
+3.times do
+  play :D6
+  sleep 0.6
+end
+play :Ef6
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+play :Ef5
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+
+play :G6
+sleep 0.6
+play :G5
+sleep 0.45
+play :G5
+sleep 0.15
+play :G6
+sleep 0.6
+play :Gf6
+sleep 0.45
+play :F6
+sleep 0.15
+
+play :E6
+sleep 0.15
+play :Ds6
+sleep 0.15
+play :E6
+
+sleep 0.30
+sleep 0.30
+play :Gs5
+sleep 0.30
+play :Cs6
+sleep 0.6
+play :Bs5
+sleep 0.45
+play :B5
+sleep 0.15
+
+play :Bf5
+sleep 0.15
+play :A5
+sleep 0.15
+play :Bf5
+sleep 0.30
+sleep 0.30
+play :Ef5
+sleep 0.30
+play :Gf5
+sleep 0.6
+play :Ef5
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+play :Ef5
+sleep 0.45
+play :Bf5
+sleep 0.1
+play :G5
+sleep 0.6
